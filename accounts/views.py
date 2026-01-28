dd4dffdc6fb6ca0462b259018de17f4a51a7fe1e
Gosia2024 <lm.sprachen@gmail.com>
2026-01-28 21:10:19 +0100

    Initial backend setup with working /api/login endpoint

---
+++ accounts/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth import authenticate
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+@api_view(['POST'])
+def login_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    if not username or not password:
+        return Response(
+            {"detail": "Username and password are required"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    user = authenticate(username=username, password=password)
+
+    if user is None:
+        return Response(
+            {"detail": "Invalid credentials"},
+            status=status.HTTP_401_UNAUTHORIZED
+        )
+
+    
+    refresh = RefreshToken.for_user(user)
+    access = refresh.access_token
+
+    response = Response(
+        {"detail": "Login successful"},
+        status=status.HTTP_200_OK
+    )
+
+    response.set_cookie(
+        key='access_token',
+        value=str(access),
+        httponly=True,
+        secure=False,   # Prduction True
+        samesite='Lax'
+    )
+
+    response.set_cookie(
+        key='refresh_token',
+        value=str(refresh),
+        httponly=True,
+        secure=False,
+        samesite='Lax'
+    )
+
+    return response

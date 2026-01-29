d2057ec93e10cc0d074c22b8428fe6e823d50b00
Gosia2024 <lm.sprachen@gmail.com>
2026-01-29 12:30:24 +0100

    feat(models): add quiz and question models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Quiz(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    video_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, related_name="questions", on_delete=models.CASCADE)
+    question_title = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class QuestionOption(models.Model):
+    question = models.ForeignKey(Question, related_name="question_options", on_delete=models.CASCADE)
+    option_text = models.CharField(max_length=255)

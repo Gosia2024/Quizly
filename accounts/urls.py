84c1ea311875c0bcfd78eb371cb5289a4fa0e08a
Gosia2024 <lm.sprachen@gmail.com>
2026-02-07 18:26:05 +0100

    Add named URLs to accounts routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 from accounts.views import logout_user, refresh_token,  register_user
 from accounts.views import login_user
 
-urlpatterns = [
-    path('register/', register_user),
-    path('login/', login_user),
-    path('logout/', logout_user),
-    path('token/refresh/', refresh_token),
+# urlpatterns = [
+#     path('register/', register_user),
+#     path('login/', login_user),
+#     path('logout/', logout_user),
+#     path('token/refresh/', refresh_token),
 
-]  
+# ]  
+urlpatterns = [
+    path('register/', register_user, name="register"),
+    path('login/', login_user, name="login"),
+    path('logout/', logout_user, name="logout"),
+    path('token/refresh/', refresh_token, name="token_refresh"),
+]

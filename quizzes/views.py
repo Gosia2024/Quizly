8f27386080c4c4091209383dcebcadc069d8c644
Gosia2024 <lm.sprachen@gmail.com>
2026-02-08 02:34:52 +0100

    Add quiz generation logic

--- quizzes/views.py
+++ quizzes/views.py
@@ -63,34 +63,6 @@ def create_quiz(request):
     return Response(serializer.data, status=201)
 
 
-# @api_view(['POST'])
-# @permission_classes([IsAuthenticated])
-# def create_quiz(request):
-#     url = request.data.get("url")
-
-#     if not url:
-#         return Response(
-#             {"detail": "URL is required"},
-#             status=status.HTTP_400_BAD_REQUEST
-#         )
-
-#     if "youtube.com" not in url and "youtu.be" not in url:
-#         return Response(
-#             {"detail": "Invalid YouTube URL"},
-#             status=status.HTTP_400_BAD_REQUEST
-#         )
-
-#     quiz = Quiz.objects.create(
-#         title="Generated Quiz",
-#         description="Quiz generated from YouTube video",
-#         video_url=url,
-#         owner=request.user
-#     )
-
-#     serializer = QuizSerializer(quiz)
-#     return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def list_quizzes(request):
@@ -99,50 +71,7 @@ def list_quizzes(request):
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-# @api_view(['GET', 'PATCH'])
-# @permission_classes([IsAuthenticated])
-# def quiz_detail(request, quiz_id):
-#     quiz = get_object_or_404(Quiz, id=quiz_id)
 
-#     # Sprawdzenie właściciela
-#     if quiz.owner != request.user:
-#         return Response(
-#             {"detail": "Access denied - quiz does not belong to user"},
-#             status=status.HTTP_403_FORBIDDEN
-#         )
-
-#     # ---------- GET ----------
-#     if request.method == 'GET':
-#         serializer = QuizSerializer(quiz)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
-#     # ---------- PATCH ----------
-#     if request.method == 'PATCH':
-#         serializer = QuizSerializer(
-#             quiz,
-#             data=request.data,
-#             partial=True
-#         )
-
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-# @api_view(['DELETE'])
-# @permission_classes([IsAuthenticated])
-# def quiz_delete(request, quiz_id):
-#     quiz = get_object_or_404(Quiz, id=quiz_id)
-
-#     # ownership check
-#     if quiz.owner != request.user:
-#         return Response(
-#             {"detail": "Access denied - quiz does not belong to user"},
-#             status=status.HTTP_403_FORBIDDEN
-#         )
-
-#     quiz.delete()
-#     return Response(status=status.HTTP_204_NO_CONTENT)
 @api_view(['GET', 'PATCH', 'DELETE'])
 @permission_classes([IsAuthenticated])
 def quiz_detail(request, quiz_id):
